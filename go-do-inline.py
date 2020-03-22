58d1451a420bb6f5c9916357eaadb20c0d79333c
-global <-global>
2020-03-22 19:41:46 -0400

    Added paramkio close

--- go-do-inline.py
+++ go-do-inline.py
@@ -7,14 +7,14 @@ import sys
 import re
 
 # Disable paging
-def DISABLE_PAGING(remote_conn):
+def DISABLE_PAGING(remoteConn):
     '''Disable paging on a Cisco router'''
 
-    remote_conn.send("terminal length 0\n")
+    remoteConn.send("terminal length 0\n")
     time.sleep(1)
 
     # Clear the buffer on the screen
-    output = remote_conn.recv(1000)
+    output = remoteConn.recv(1000)
 
     return output
 
@@ -27,40 +27,44 @@ if __name__ == '__main__':
     command = sys.argv[2]
 
     # Create instance of SSHClient object
-    remote_conn_pre = paramiko.SSHClient()
+    remoteConnPre = paramiko.SSHClient()
 
     # Automatically add untrusted hosts (make sure okay for security policy in your environment)
-    remote_conn_pre.set_missing_host_key_policy(
+    remoteConnPre.set_missing_host_key_policy(
         paramiko.AutoAddPolicy())
     
     # Initiate SSH connection
-    remote_conn_pre.connect(ip, username=username, password=password, look_for_keys=False, allow_agent=False)
-    remote_conn = remote_conn_pre.invoke_shell()
+    remoteConnPre.connect(ip, username=username, password=password, look_for_keys=False, allow_agent=False)
+    remoteConn = remoteConnPre.invoke_shell()
     print("Interactive SSH Connection Established to %s" % ip)
 
     # Enter privileged exec mode
-    output = remote_conn.recv(1000000)
+    output = remoteConn.recv(1000000)
     outputStr = output.decode('utf-8')
     if (outputStr.endswith('#')):
         print("In privileged mode.")
     else:
         print("Attepting to enter privileged mode.")
-        remote_conn.send("enable")
-        remote_conn.send("\n")
-        remote_conn.send(enablePassword)
+        remoteConn.send("enable")
+        remoteConn.send("\n")
+        remoteConn.send(enablePassword)
     
     # Show current prompt
     print(outputStr)
 
     # Disable paging
-    DISABLE_PAGING(remote_conn)
+    DISABLE_PAGING(remoteConn)
 
     # Send command
-    remote_conn.send(command)
-    remote_conn.send("\n")
+    remoteConn.send(command)
+    remoteConn.send("\n")
     time.sleep(2)
-    output = remote_conn.recv(1000000)
+    output = remoteConn.recv(1000000)
     outputStr = output.decode('utf-8')
-    
+
     # Show command result
     print(outputStr)
+
+    # Close connection
+    remoteConn.close()
+    print("Interactive SSH Connection to %s closed." %ip)
